e0fe4220a2bf84cb0e4169fb98832659be969cfc
Gianfranco Iacono <iacono.gian13@gmail.com>
2025-03-09 20:15:38 -0300

    refactor: agregar documentación a las funciones getUvRiskLevel y getUvDescription en utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,11 @@
 import { UvRiskLevels } from "../types/weatherTypes";
 import { uvDescriptions } from "./constants";
 
+/**
+ * Obtiene el nivel de riesgo UV basado en el índice UV.
+ * @param index - Índice UV.
+ * @returns Nivel de riesgo UV.
+ */
 export const getUvRiskLevel = (index: number): UvRiskLevels => {
   if (index < 0) return UvRiskLevels.unknown;
   if (index <= 2) return UvRiskLevels.low;
@@ -8,6 +13,11 @@ export const getUvRiskLevel = (index: number): UvRiskLevels => {
   return UvRiskLevels.high;
 };
 
+/**
+ * Obtiene la descripción del riesgo UV basado en el índice UV.
+ * @param index - Índice UV.
+ * @returns Descripción del riesgo UV.
+ */
 export const getUvDescription = (index: number): string => {
   if (index < 0) return uvDescriptions.unknown;
   if (index <= 2) return uvDescriptions.low;
