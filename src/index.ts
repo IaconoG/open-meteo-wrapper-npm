a33a371d01eeb7aefa0946ceab771eb33fa3c8dc
Gianfranco Iacono <iacono.gian13@gmail.com>
2025-03-13 21:26:26 -0300

    refactor: reorganizar exportaciones en index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,9 @@ export { fetchWeather } from "@/services/weatherService";
  * meteorológicos.
  */
 export type {
-  FetchError,
   FetchWeatherProps,
-  StructureWeatherData,
-  DailyWeatherData,
-  HourlyWeatherData,
-} from "@/types/weatherTypes";
-
-/**
- * @module types/weatherTypes
- * Estructura de los datos meteorológicos obtenidos de la API.
- */
-export {
-  ErrorType,
-  HourlyParams,
   DailyParams,
-  WeatherDescriptions,
-  UvRiskLevels,
+  HourlyParams,
+  StructureWeatherData,
+  FetchError,
 } from "@/types/weatherTypes";
